49a466d6756750b6dbe38ad44568fe12aa0f550b
pan <paan@ik.me>
2026-08-18 17:00:54 +0200

    test(lifecycle): add happy path integration test

---
+++ tests/lifecycle.rs
@@ -0,0 +1,105 @@
+mod common;
+
+#[cfg(target_os = "linux")]
+mod lifecycle_test {
+    use super::common::{cleanup, get_binary_path, get_state_file_path};
+    use std::fs::read_to_string;
+    use std::process::Command;
+
+    #[test]
+    fn lifecycle_happy_path() {
+        let id = "happy_path";
+        cleanup(id);
+
+        Command::new(&get_binary_path())
+            .args(["create", id, "tests/lifecycle_config"])
+            .spawn()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        let output = read_to_string(&get_state_file_path(id)).unwrap();
+
+        assert!(output.contains("\"status\":\"created\""));
+
+        Command::new(&get_binary_path())
+            .args(["start", id])
+            .spawn()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        let output = read_to_string(&get_state_file_path(id)).unwrap();
+
+        assert!(output.contains("\"status\":\"running\""));
+
+        Command::new(&get_binary_path())
+            .args(["pause", id])
+            .spawn()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        let output = read_to_string(&get_state_file_path(id)).unwrap();
+
+        assert!(output.contains("\"status\":\"paused\""));
+
+        Command::new(&get_binary_path())
+            .args(["resume", id])
+            .spawn()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        let output = read_to_string(&get_state_file_path(id)).unwrap();
+
+        assert!(output.contains("\"status\":\"running\""));
+
+        let command = Command::new(&get_binary_path())
+            .args(["exec", id, "/bin/echo OK"])
+            .status()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+
+        assert!(command.success());
+
+        Command::new(&get_binary_path())
+            .args(["kill", id])
+            .spawn()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+        let output = read_to_string(&get_state_file_path(id)).unwrap();
+
+        assert!(output.contains("\"status\":\"stopped\""));
+
+        let command = Command::new(&get_binary_path())
+            .args(["delete", id])
+            .status()
+            .unwrap();
+
+        assert!(command.success());
+    }
+
+    #[test]
+    fn natural_direct_exit_success_test() {
+        let id = "natural_direct_exit_success_test";
+        cleanup(id);
+
+        let _create = Command::new(&get_binary_path())
+            .args(["create", id, "tests/lifecycle_config"])
+            .spawn()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_millis(500));
+
+        Command::new(&get_binary_path())
+            .args(["start", id])
+            .spawn()
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_secs(35));
+
+        let output = read_to_string(&get_state_file_path(id)).unwrap();
+        assert!(output.contains("\"status\":\"stopped\""));
+
+        cleanup(id);
+    }
+}
