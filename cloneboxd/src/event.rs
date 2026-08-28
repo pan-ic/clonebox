31b7da4913b4eaef57136641712d1fdef57dc89c
pan <paan@ik.me>
2026-08-28 20:53:08 +0200

    feat(daemon): add events listener, missed on previous commit

---
+++ cloneboxd/src/event.rs
@@ -0,0 +1,84 @@
+use anyhow::Context;
+use std::{
+    collections::HashMap,
+    sync::{
+        Arc,
+        Mutex,
+        Weak,
+    },
+};
+use tokio::{
+    io::{BufReader, AsyncBufRead, AsyncBufReadExt},
+    net::{
+        UnixListener,
+        UnixSocket,
+        UnixStream,
+    },
+};
+use clonebox_core::{
+    state::{
+        ContainerState,
+        State,
+    },
+    event::Event,
+};
+
+use crate::daemon::ContainerEntry;
+
+pub(crate) async fn get_listen_sk(path: &str) -> anyhow::Result<UnixListener> {
+    let _ = std::fs::remove_file(path);
+
+    Ok(UnixListener::bind(path).unwrap())
+}
+
+fn update_status(event: Event,
+    containers: &Arc<Mutex<HashMap<String, ContainerEntry>>>) -> anyhow::Result<()> {    
+    let mut h = containers.lock().unwrap();
+
+    if let Some(entry) = h.get_mut(&event.get_id()) {
+        entry.state = event.get_state();
+        if entry.state != ContainerState::Creating {
+            if let Some(tx) = entry.created_waiter.take() { 
+                let _ = tx.send(()); 
+            }
+        }
+        if entry.state == ContainerState::Stopped {
+            let code = event.get_exit_code().unwrap_or_default();
+            for tx in std::mem::take(&mut entry.exit_waiter) {
+                let _ = tx.send(code);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+pub(crate) async fn event_loop(listener: UnixListener, containers: &Arc<Mutex<HashMap<String, ContainerEntry>>>) -> anyhow::Result<()> {
+    let containers_outer = Arc::clone(&containers);
+        
+    tokio::spawn(async move {
+            
+        loop {
+            let containers_inner = Arc::clone(&containers_outer);
+            let (conn, _) = listener.accept().await.unwrap();
+            let mut lines = BufReader::new(conn).lines();
+                
+            tokio::spawn(async move {
+
+                while let Ok(Some(line)) = lines.next_line().await {
+                    if let Ok(e) = serde_json::from_str::<Event>(&line) {
+                        let _ = update_status(e, &containers_inner);
+                    } else {
+                        eprintln!("event: event_loop: failed to deserialize");
+                    }
+                } 
+            });
+        }
+    });
+
+    Ok(())
+}
+
+pub(crate) fn get_app_data_path() -> String {
+    String::from("/run/clonebox/event.sk")
+}
