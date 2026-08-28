5fe4154e6f6496d6c53956cdf7f2e38f4a4cfa39
pan <paan@ik.me>
2026-08-28 20:40:33 +0200

    feat(daemon): start/kill/delete handlers

--- cloneboxd/src/bin/clonebox_test_client.rs
+++ cloneboxd/src/bin/clonebox_test_client.rs
@@ -1,5 +1,12 @@
+use tonic::{transport::Server, Request, Response, Status};
+use tonic::IntoRequest;
 use clonebox_tasks::clonebox_tasks_client::{CloneboxTasksClient};
-use clonebox_tasks::StateRequest;
+use clonebox_tasks::{
+    CreateRequest,
+    StartRequest,
+    DeleteRequest,
+    KillRequest,
+};
 
 pub mod clonebox_tasks {
     tonic::include_proto!("clonebox_tasks");
@@ -7,15 +14,40 @@ pub mod clonebox_tasks {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = CloneboxTasksClient::connect("http://[::1]:50051").await?;
+    let mut client = CloneboxTasksClient::connect("/run/clonebox/clonebox.sock").await?;
 
-    let request = tonic::Request::new(StateRequest {
-        container_id: "test".into(),
-    });
+    let create_req = tonic::Request::new(
+        CreateRequest {
+            container_id: String::from("test"),
+            config_path: String::from("/home/debian/clonebox/clonebox/config"),
+        }
+    );
+    let start_req = tonic::Request::new(
+        StartRequest {
+            container_id: String::from("test"),
+        }
+    );
+    let delete_req = tonic::Request::new(
+        DeleteRequest {
+            container_id: String::from("test"),
+            force: false,
+        }
+    );
+    let kill_req = tonic::Request::new(
+        KillRequest {
+            container_id: String::from("test"),
+        }
+    );
 
-    let response = client.state(request).await?;
+    let create_resp = client.create(create_req).await?;
+    let start_resp = client.start(start_req).await?;
+    let kill_resp = client.kill(kill_req).await?;
+    let delete_resp = client.delete(delete_req).await?;
 
-    println!("RESPONSE={:?}", response);
+    println!("CREATE={:?}", create_resp);
+    println!("START={:?}", start_resp);
+    println!("KILL={:?}", kill_resp);
+    println!("DELETE={:?}", delete_resp);
 
     Ok(())
 } 
--- cloneboxd/src/daemon.rs
+++ cloneboxd/src/daemon.rs
@@ -164,4 +164,80 @@ impl CloneboxTasks for Cloneboxd {
             },
         }
     }
+
+    async fn start(&self, request: Request<StartRequest>) -> anyhow::Result<Response<StartResponse>, Status> {
+        debug!("Request: {:#?}", request);
+        let args = request.into_inner();
+        info!("State querry for {}", args.container_id);
+       
+        //start(&args.container_id).inspect_err(|e| error!(id = %args.container_id, %e, "start failed"))?;
+        match start(&args.container_id) {
+            Ok(_) => {
+                {
+                    let mut containers = self.containers.lock().unwrap();
+                    if let Some(entry) = containers.get_mut(&args.container_id) {
+                        entry.state = ContainerStateCore::Running;
+                    }
+                }
+                info!("{} successfully started", args.container_id);
+                Ok(Response::new(StartResponse {}))
+            },
+            Err(e) => {
+                let err = e.to_string();
+                error!("Start: {:#?}", err);
+                Err(Status::internal(err))
+            },
+        }
+    }
+
+    async fn kill(&self, request: Request<KillRequest>) -> anyhow::Result<Response<KillResponse>, Status> {
+        debug!("Request: {:#?}", request);
+        let args = request.into_inner();
+        info!("Kill querry for {}", args.container_id);
+        
+        match kill(&args.container_id) {
+            Ok(_) => {
+                {
+                    let mut containers = self.containers.lock().unwrap();
+                    if let Some(entry) = containers.get_mut(&args.container_id) {
+                        entry.state = ContainerStateCore::Stopped;
+                    }
+                }
+                info!("{} successfully killed", args.container_id);
+                Ok(Response::new(KillResponse {}))
+            }
+            Err(e) => {
+                let err = e.to_string();
+                error!("Kill: {:#?}", err);
+                Err(Status::internal(err))
+            },
+        }
+    }
+
+    async fn delete(&self, request: Request<DeleteRequest>) -> anyhow::Result<Response<DeleteResponse>, Status> {
+        debug!("Request: {:#?}", request);
+        let args = request.into_inner();
+        info!("Delete querry for {}", args.container_id);
+
+        match delete(&args.container_id, args.force) {
+            Ok(_) => {
+                {
+                    let mut containers = self.containers.lock().unwrap();
+                    if let Some(entry) = containers.get_mut(&args.container_id) {
+                        if entry.state == ContainerStateCore::Stopped {
+                            containers.remove_entry(&args.container_id);
+                        }
+                    }
+                }
+                info!("{} successfully deleted", args.container_id);
+                Ok(Response::new(DeleteResponse { }))
+            },
+            Err(e) => {
+                let err = e.to_string();
+                error!("Delete: {:#?}", err);
+                Err(Status::internal(err))
+            }
+        }
+    }
+
 }
